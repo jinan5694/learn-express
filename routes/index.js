ccfbe4c6c7b7c5caae24b24a1fd5f005849dcac3
nan.ji <nan.ji@sim.com>
2015-04-24 11:04:02 +0800

    login

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,22 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* login demo */
+/*
+* res.render('login', {layout: false}); 可以设置false来禁用默认的模板
+* res.render('login', {layout: 'login'}); 可以设置模板名称来使用其他模板
+*/
+router.get('/login', function(req, res) {
+	res.render('login', {layout: 'login'});
+});
+
+router.post('/login', function(req, res) {
+	console.log('post login');
+	console.log(req.body); // req.body 可以取到form中的参数
+	res.render('login', {layout: 'login'});
+});
+
+
 /* list demo */
 
 router.get('/list', function(req, res) {
